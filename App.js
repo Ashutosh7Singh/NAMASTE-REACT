7b2d3d4a408dee03e197a82ae9ce9fa47d7642c9
HULK7ASHU <8804ashutosh@gmail.com>
2023-11-12 20:56:13 +0530

    update

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent=React.createElement("div",{id:"parent"},[
-  React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I am h1 tag"),
-    React.createElement("h2",{},"I am h2 tag")
-  ]),
-  React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I am h1 tag"),
-    React.createElement("h2",{},"I am h2 tag")
-  ]),
-]);
+//reacat Element
 
-console.log(parent);
+
+
+//jsx (transppiled before ir reacher the js engine)-Parcel-Babel;
+//transpiled means condvert the cont in machine language
+//jsx=>React.createElement=>ReactElement-js Object=>HTMLElement(render)
+ 
+const Title =()=> (
+  <h1 className="head" tabIndex="5"> 
+    Namatate React using jsx 🚀 qowerty0
+    </h1>
+    );
+
+//react function Component 
+const  HeadingComponent = () =>(
+  <div id="container">
+  <h1>{400+300}</h1>
+  {Title()}
+  <Title/>
+  <Title/>
+  <Title></Title>
+
+  {Title}
+
+  <h1 className="head">Namaste React function Component</h1>
+  </div> 
+)
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
+
+
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+npx parcel build index.html
+we got error then
+remove "main": "App.js", from package.json
+
+ npx parcel build index.html
+ when we run this we got new dist and parcel-cache file  or we can say that this ifle production build file and if we want to give nay one to our file we give this
+  so we dont need to put then on git 
+
+
+eslint
+bracket cololizer
+better Comment
